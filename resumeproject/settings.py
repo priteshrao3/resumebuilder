9350eba6aa7b913e567bdebba6ce33f9d55d7b78
priteshrao3 <priteshrao3@gmail.com>
2024-06-18 12:25:28 +0530

    static saetting updated

--- resumeproject/settings.py
+++ resumeproject/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = "resumeproject/static/"
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'resumeproject/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
