309c74af59a30d5a00c39528581b79aa96785beb
priteshrao3 <priteshrao3@gmail.com>
2024-06-18 23:49:07 +0530

    home page updated with tailwind css

---
+++ core/custom_middleware.py
@@ -0,0 +1,24 @@
+# custom_middleware.py
+
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+class AutoLoginMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        # Check if the request is for the admin panel
+        if request.path.startswith(reverse('admin:index')):
+            # Check if the user is not authenticated
+            if not request.user.is_authenticated:
+                try:
+                    # Try to get the 'admin' user
+                    user = User.objects.get(username='admin')
+                    request.user = user  # Set the user for the request
+                except User.DoesNotExist:
+                    pass  # Handle the case if 'admin' user doesn't exist
+
+        response = self.get_response(request)
+        return response
