ade2e744a7c2536cc0a6678de7484d3797c62afe
priteshrao3 <priteshrao3@gmail.com>
2024-06-19 09:14:04 +0530

    update pdf css

--- core/custom_middleware.py
+++ core/custom_middleware.py
@@ -1,5 +1,3 @@
-# custom_middleware.py
-
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.http import HttpResponseRedirect
@@ -9,16 +7,13 @@ class AutoLoginMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Check if the request is for the admin panel
         if request.path.startswith(reverse('admin:index')):
-            # Check if the user is not authenticated
             if not request.user.is_authenticated:
                 try:
-                    # Try to get the 'admin' user
                     user = User.objects.get(username='pritesh')
-                    request.user = user  # Set the user for the request
+                    request.user = user
                 except User.DoesNotExist:
-                    pass  # Handle the case if 'admin' user doesn't exist
+                    pass
 
         response = self.get_response(request)
         return response
